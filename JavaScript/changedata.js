b4ec6ac367f1b5eca29ce68009495a88754c6807
odaka1994 <60916150+odaka1994@users.noreply.github.com>
2020-02-11 19:20:59 +0900

    Update changedata.js

--- JavaScript/changedata.js
+++ JavaScript/changedata.js
@@ -1,19 +1,12 @@
-
-
 function isCheck(){
-	
 	var pass,name;
 	pass = document.getElementById("input1").value.replace(/\s+/g, "");
 	name = document.getElementById("input2").value.replace(/\s+/g, "");
-	
-	//alert (pass + "：" + name);
-	
+
 	if (pass == "" && name == ""){
 		alert("パスワードまたは名前を入力してください。");
 		return false
 	}else{
 		return true
 	}
-	
 }
-
