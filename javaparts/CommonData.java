11e44e8c48747a8906b733a22ce4afd647ed4d88
odaka1994 <60916150+odaka1994@users.noreply.github.com>
2020-02-11 19:25:54 +0900

    Update CommonData.java

--- javaparts/CommonData.java
+++ javaparts/CommonData.java
@@ -1,12 +1,11 @@
 package javaparts;
 
-
 public class CommonData{
 	
 	//DB
 	public static final String URL = "jdbc:mysql://";
-	public static final String USERNAME = "uname";
-	public static final String PASSWORD = "œœœœ";
+	public static final String USERNAME = "username";
+	public static final String PASSWORD = "xxxx";
 	public static final String tableName = "tableA";
 	
 }
