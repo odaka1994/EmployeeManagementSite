a25a5ea96bca5f336c18772cc182edd741d84c7e
odaka1994 <60916150+odaka1994@users.noreply.github.com>
2020-02-11 19:22:34 +0900

    Update login.js

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -1,19 +1,13 @@
-
-
 function isCheck(){
 	
 	var userid,pass;
 	userid = deleteBlank(document.getElementById("input1").value);
 	pass = deleteBlank(document.getElementById("input2").value);
 	
-	//alert (userid + "：" + pass);
-	
 	if (userid == "" && pass == ""){
 		alert("ユーザーIDとパスワードを入力してください。");
 		return false
 	}else{
 		return true
-	}
-	
+	}	
 }
-
